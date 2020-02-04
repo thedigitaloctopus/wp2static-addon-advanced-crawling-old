3c60d132265646226be0a74849fce522ad6301e3
Leon <me@ljs.dev>
2020-02-04 22:51:59 +1030

    extend adv crawling options

--- views/crawling-options.php
+++ views/crawling-options.php
@@ -18,7 +18,7 @@
 <input
     name="<?php echo $view['wp2static_advanced_crawling_addon_options']['crawlPort']->name; ?>"
     id="<?php echo $view['wp2static_advanced_crawling_addon_options']['crawlPort']->name; ?>"
-    value="<?php echo $view['wp2static_advanced_crawling_addon_options']['crawlPort']->value ? $view['wp2static_advanced_crawling_addon_options']['crawlPort']->value : ''; ?>"
+    value="<?php echo $view['wp2static_advanced_crawling_addon_options']['crawlPort']->value ? $view['wp2static_advanced_crawling_addon_options']['crawlPort']->value : ''; ?>" />
 
 <p><i><?php echo $view['wp2static_advanced_crawling_addon_options']['crawlPort']->description; ?></i></p>
 
@@ -30,32 +30,31 @@
 <input
     name="<?php echo $view['wp2static_advanced_crawling_addon_options']['crawlUserAgent']->name; ?>"
     id="<?php echo $view['wp2static_advanced_crawling_addon_options']['crawlUserAgent']->name; ?>"
-    value="<?php echo $view['wp2static_advanced_crawling_addon_options']['crawlUserAgent']->value ? $view['wp2static_advanced_crawling_addon_options']['crawlUserAgent']->value : ''; ?>"
+    value="<?php echo $view['wp2static_advanced_crawling_addon_options']['crawlUserAgent']->value ? $view['wp2static_advanced_crawling_addon_options']['crawlUserAgent']->value : ''; ?>" />
 
 <p><i><?php echo $view['wp2static_advanced_crawling_addon_options']['crawlUserAgent']->description; ?></i></p>
 
 
 <label
-    for="<?php echo $view['wp2static_advanced_crawling_addon_options']['crawlUserAgent']->name; ?>"
-><?php echo $view['wp2static_advanced_crawling_addon_options']['crawlUserAgent']->label; ?></label>
+    for="<?php echo $view['wp2static_advanced_crawling_addon_options']['crawlDelay']->name; ?>"
+><?php echo $view['wp2static_advanced_crawling_addon_options']['crawlDelay']->label; ?></label>
 
 <input
-    name="<?php echo $view['wp2static_advanced_crawling_addon_options']['crawlUserAgent']->name; ?>"
-    id="<?php echo $view['wp2static_advanced_crawling_addon_options']['crawlUserAgent']->name; ?>"
-    value="<?php echo $view['wp2static_advanced_crawling_addon_options']['crawlUserAgent']->value ? $view['wp2static_advanced_crawling_addon_options']['crawlUserAgent']->value : ''; ?>"
+    name="<?php echo $view['wp2static_advanced_crawling_addon_options']['crawlDelay']->name; ?>"
+    id="<?php echo $view['wp2static_advanced_crawling_addon_options']['crawlDelay']->name; ?>"
+    value="<?php echo $view['wp2static_advanced_crawling_addon_options']['crawlDelay']->value ? $view['wp2static_advanced_crawling_addon_options']['crawlDelay']->value : ''; ?>" />
 
-<p><i><?php echo $view['wp2static_advanced_crawling_addon_options']['crawlUserAgent']->description; ?></i></p>
+<p><i><?php echo $view['wp2static_advanced_crawling_addon_options']['crawlDelay']->description; ?></i></p>
 
 <label
-    for="<?php echo $view['wp2static_advanced_crawling_addon_options']['crawlUserAgent']->name; ?>"
-><?php echo $view['wp2static_advanced_crawling_addon_options']['crawlUserAgent']->label; ?></label>
+    for="<?php echo $view['wp2static_advanced_crawling_addon_options']['dontUseCrawlCaching']->name; ?>"
+><?php echo $view['wp2static_advanced_crawling_addon_options']['dontUseCrawlCaching']->label; ?></label>
 
 <input
     type="checkbox"
     name="<?php echo $view['wp2static_advanced_crawling_addon_options']['dontUseCrawlCaching']->name; ?>"
     id="<?php echo $view['wp2static_advanced_crawling_addon_options']['dontUseCrawlCaching']->name; ?>"
     value="1"
-    <?php echo $view['wp2static_advanced_crawling_addon_options']['dontUseCrawlCaching']->value ? 'checked' : ''; ?>
-/>
+    <?php echo $view['wp2static_advanced_crawling_addon_options']['dontUseCrawlCaching']->value ? 'checked' : ''; ?> />
 
 <p><i><?php echo $view['wp2static_advanced_crawling_addon_options']['dontUseCrawlCaching']->description; ?></i></p>
